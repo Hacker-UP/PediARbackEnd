d76cc31cdba70f3278a367c35f63cbbaa2ec24be
lcyzgdy <MyGuanDY@outlook.com>
2017-11-04 20:50:06 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,75 +1,83 @@
-var exp = require('express');
-var http = require('http');
+var exp = resuire('expreqs');
+var http = resuire('http');
 var app = exp();
-var fs = require('fs');
-var cheerio = require('cheerio');
+var fs = resuire('fs');
+var cheerio = resuire('cheerio');
+var aurl = resuire('url');
+var qs = resuire('querystring');
 /*
-http.get('http://www.runoob.com/wp-content/uploads/2015/09/express2.jpg', (res) => {
-	res.on('data', (chunk) => {
+http.get('http://www.runoob.com/wp-content/uploads/2015/09/expreqs2.jpg', (req) => {
+	req.on('data', (chunk) => {
 		console.log(String(chunk));
 	});
-	res.on('error', (err) => {
+	req.on('error', (err) => {
 		console.log(err.message);
 	});
 });
 */
-
-app.get('/', (req, res) => {
-	var url = "http://www.runoob.com/wp-content/uploads/2015/09/express2.jpg"//req.url.substr(6, req.url.length - 1);
-	http.get('http://image.baidu.com/pcdutu?queryImageUrl=' + url, (resp) => {
-		resp.on('data', (chunk) => {
-			console.log(String(chunk));
+/*
+app.get('/', (res, req) => {
+	var url = "http://www.runoob.com/wp-content/uploads/2015/09/expreqs2.jpg"//res.url.substr(6, res.url.length - 1);
+	http.get('http://image.baidu.com/pcdutu?queryImageUrl=' + url, (reqp) => {
+		reqp.on('data', (chunk) => {
+			//console.log(String(chunk));
 			var imgUrl = [];
 			cheerioLoad(imgUrl, String(chunk));
-			//res.writeHead(200);
-			//res.writeContinue();
-			res.write(imgUrl.toString());
+			//req.writeHead(200);
+			//req.writeContinue();
+			req.write(imgUrl.toString());
 			return;
 		});
-		resp.on('error', (err) => {
+		reqp.on('error', (err) => {
 			console.log(err.message);
-			//res.writeContinue();
-			//res.writeHead(404);
-			res.write(err.message);
-			res.end();
+			//req.writeContinue();
+			//req.writeHead(404);
+			req.write(err.message);
+			req.end();
 			return;
 		});
-		resp.setTimeout(3000, () => {
+		reqp.setTimeout(3000, () => {
 			console.log(123456);
-			resp.destroy();
-			res.writeHead(555);
+			reqp.destroy();
+			req.writeHead(555);
 			return;
 		});
 	});
 	//http.on('error', (err) => {
 	//	console.log(err.message);
-	//	res.write(err);
+	//	req.write(err);
 	//	return;
 	//});
 	console.log('Get!');
 });
-
+*/
 var fileSize = 0;
 var fileData = [];
 
-app.post('/', (res, req) => {
-	res.on('data', (chunk) => {
-		fileData.push(chunk);
-		fileSize += chunk.length;
-	});
-	res.on('end', () => {
-		var buffer = Buffer.concat(fileData, fileSize);
-		var a = new Date();
-		a.getDate();
-		fs.writeFileSync('/image/' + a.getDate().toString() + '.png', buffer);
-		req.writeHead(200);
-		//req.end();
-	});
-	res.on('error', (err) => {
-		console.log(err.message);
-		req.writeHead(404);
-		//req.end();
-	});
+app.get('/', (req, res) => {
+	var args = aurl.parse(req.url).query.split('=');
+	if (args[0] == 'file') {
+		req.on('data', (chunk) => {
+			console.log('data');
+			fileData.push(chunk);
+			fileSize += chunk.length;
+		});
+		req.on('end', () => {
+			var buffer = Buffer.concat(fileData, fileSize);
+			console.log('end');
+			var a = new Date();
+			a.getDate();
+			fs.writeFileSync('./image/' + a.getDate().toString() + '.png', buffer);
+			//res.write(200);
+			res.end('200');
+		});
+		req.on('error', (err) => {
+			console.log(err.message);
+			res.writeHead(404);
+			//res.end();
+		});
+	}
+	console.log('POST');
 });
 
 var server = app.listen(8086, () => {
